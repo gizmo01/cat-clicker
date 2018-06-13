4a84bbcfb2345d1423244cd2f1d558445b646dd1
Mario Amore <quaaludes@gmail.com>
2018-06-13 19:44:29 +0800

    New version of cat clicker

--- premium-version/app.js
+++ premium-version/app.js
@@ -1,6 +1,7 @@
 // M O D E L - only data is present here in the model
 let model = {
   currentCat: null,
+  showAdminPanel: false,
   cats: [{
       name: "Dio",
       clicks: 0,
@@ -32,7 +33,8 @@ let model = {
 // O C T O P U S
 let octopus = {
   init: function() {
-    listView.render();
+    listView.init();
+    adminView.init();
   },
   getCats: function() {
     return model.cats;
@@ -44,15 +46,72 @@ let octopus = {
     return model.currentCat;
   },
   addClicks: function() {
-    model.currentCat.clicks++
+    model.currentCat.clicks++;
+  },
+  showAdminPanel: function() {
+    document.querySelector('.form-container').classList.remove('hide');
+    model.showAdminPanel = false;
+  },
+  hideAdminPanel: function() {
+    document.querySelector('.form-container').classList.add('hide');
+    model.showAdminPanel = true;
+  },
+  updateValues: function() {
+    let name = document.getElementById('name').value;
+    if (name !== "") {
+      model.currentCat.name = name;
+      listView.init();
+      catView.render();
+      document.getElementById('name').value = "";
+    }
+    let url = document.getElementById('url').value;
+    if (url !== "") {
+      model.currentCat.src = url;
+      catView.render();
+      document.getElementById('url').value = "";
+    }
+
+    let click = document.getElementById('clicks').value;
+    if (click !== "") {
+      model.currentCat.clicks = click;
+      catView.render();
+      document.getElementById('clicks').value = "";
+    }
+    this.hideAdminPanel();
+
+  }
+};
+//completed handling of admin button adn cancel button
+
+// A D M I N V I E W
+let adminView = {
+  init: function() {
+    let admin = document.querySelector('.admin');
+    let cancel = document.querySelector('.cancel');
+    let save = document.querySelector('.save');
+    admin.addEventListener('click', function() {
+      octopus.showAdminPanel();
+    });
+    cancel.addEventListener('click', function() {
+      octopus.hideAdminPanel();
+    });
+    save.addEventListener('click', function() {
+      octopus.updateValues();
+    });
+  },
+  render: function() {
+    document.querySelector('.form-container').classList.toggle('hide');
   }
 };
 
-
 // L I S T V I E W
 let listView = {
+  init: function() {
+    listView.render();
+  },
   render: function() {
     let catArr = octopus.getCats();
+    document.querySelector('.list').innerHTML = "";
     for (cat of catArr) {
       let elem = document.createElement('button');
       elem.textContent = cat.name;
@@ -83,5 +142,8 @@ let catView = {
     });
   }
 };
+
+
+
 
 octopus.init();
